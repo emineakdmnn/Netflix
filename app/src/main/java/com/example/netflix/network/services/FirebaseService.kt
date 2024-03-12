54e426d8899b3d5b02619620962408e92e07d63f
Emine Akduman <eakdmn@gmail.com>
2024-03-12 22:53:35 +0300

    service added.

--- app/src/main/java/com/example/netflix/network/services/FirebaseService.kt
+++ app/src/main/java/com/example/netflix/network/services/FirebaseService.kt
@@ -1,4 +1,10 @@
 package com.example.netflix.network.services
 
-class FirebaseService {
+import com.example.netflix.network.models.FeedResponse
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface FirebaseService {
+    @GET("p%2Fnetflix%2Fapi%2Ffeed%2F{page}.json?alt=media")
+    suspend fun fetchFeedItems(@Path("page") page: Int): FeedResponse
 }
